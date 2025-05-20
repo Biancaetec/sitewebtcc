94bc8d324b59a99a3e63ffd161add8babbea2e76
Biancaetec <161024767+Biancaetec@users.noreply.github.com>
2025-05-20 11:34:43 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('LOCAL_PATH','http://localhost/bdtcc/');
+define('HOST', 'localhost');
+define('PORT','3306');
+define('DBNAME', 'siterestoon');
+define('USER', 'root');
+define('PASSWORD', '');
+
+define('CHARSET','utf8');
